d0118d692c926bc2b4cb36f61eb0113a5bf1f2e8
kor <kor@dwarshuis.com>
2024-07-12 12:18:55 +0200

    Remove three columns block temporarly

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
     >
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <p className="text-center">Personal learning environment and consensus building tool</p>
       </main>
     </Layout>
   );
